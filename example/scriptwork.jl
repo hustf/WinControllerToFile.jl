c059d8781419e89568007e6e76e2058b226b5005
hustf <hustf@users.noreply.github.com>
2020-04-10 18:35:11 +0200

    modified:   example/scriptwork.jl          Slight extension modified:   src/WinControllerToFile.jl     Write status in one operation

--- example/scriptwork.jl
+++ example/scriptwork.jl
@@ -1,7 +1,10 @@
 
 using WinControllerToFile
 hids = hid_dict()
+hidvec = hid_vector()
+hidvec[1]
 h = hids["\\\\?\\hid#vid_044f&pid_b10a#6&396faf92&0&0000#{4d1e55b2-f16f-11cf-88cb-001111000030}"] 
 set_raw_data_handler(1, h)
 open_hid(h)
 is_plugged(h)
+usisubscribe()
